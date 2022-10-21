a448ee48a2418310c719fb01060aad46c10acdc0
Harold_Mesa <harold.mesa@icloud.com>
2022-10-21 15:45:32 -0500

    Add View Models.

---
+++ Pierre.Solution/ViewModels/LoginViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RecipeBox.ViewModels
+{
+  public class LoginViewModel
+  {
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    
+  }
+}
