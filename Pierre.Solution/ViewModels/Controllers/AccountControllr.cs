b76ff66b90f2361cf23ebc786182e93ef971dd1b
Harold_Mesa <harold.mesa@icloud.com>
2022-10-21 16:40:06 -0500

    Add HomeController.cs, AccountController.cs, and TreatsController.cs

---
+++ Pierre.Solution/ViewModels/Controllers/AccountControllr.cs
@@ -0,0 +1,80 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using RecipeBox.Models;
+using System.Threading.Tasks;
+using PierresBakery.ViewModels;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using System;
+
+namespace PierresBakery.Controllers
+{
+	public class AccountController : Controller
+	{
+		private readonly PierresBakeryContext _db;
+		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly SignInManager<ApplicationUser> _signInManager;
+
+		public AccountController (UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, PierresBakeryContext db)
+		{
+			_userManager = userManager;
+			_signInManager = signInManager;
+			_db = db;
+		}
+
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Register()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> Register (RegisterViewModel model)
+		{
+			var user = new ApplicationUser { UserName = model.UserName, FullName = model.FullName, LastTimeVisiting = model.LastTimeVisiting};
+			IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+			if (result.Succeeded)
+			{
+				await _userManager.AddClaimAsync(user, new Claim("FullName", user.FullName));
+				await _userManager.AddClaimAsync(user, new Claim("LastTimeVisiting", user.LastTimeVisiting.ToString("MM/dd/yyyy")));
+				await _signInManager.SignInAsync(user, isPersistent: true);
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				return View();
+			}
+		}
+
+		public ActionResult Login()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> Login(LoginViewModel model)
+		{
+			Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, isPersistent: true, lockoutOnFailure: false);
+			if (result.Succeeded)
+			{
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				return View();
+			}
+		}
+
+    [HttpPost]
+    public async Task<ActionResult> LogOff()
+    {
+      await _signInManager.SignOutAsync();
+      return RedirectToAction("Index");
+    }
+
+	}
+}
---
+++ Pierre.Solution/ViewModels/Controllers/TreatsController.cs
@@ -0,0 +1,136 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using PierresBakery.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace PierresBakery.Controllers
+{
+  [Authorize]
+  public class TreatsController : Controller
+  {
+    private readonly PierresBakeryContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public TreatsController(UserManager<ApplicationUser> userManager, PierresBakeryContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+		public async Task<ActionResult> Index(string sortOrder)
+		{
+			var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+			var currentUser = await _userManager.FindByIdAsync(userId);
+			switch (sortOrder)
+			{
+				case "price":
+					var sortByPrice = _db.Treats.Where(entry => entry.User.Id == currentUser.Id).OrderByDescending(treat => treat.Price).ToList();
+					return View(sortByPrice);
+				default:
+					var sortByName = _db.Treats.Where(entry => entry.User.Id == currentUser.Id).OrderBy(treat => treat.Treat).ToList();
+					return View(sortByName);
+			}
+		}
+
+		public async Task<ActionResult> Create()
+		{
+			var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+			var currentUser = await _userManager.FindByIdAsync(userId);
+			List<Flavor> userFlavors = _db.Flavors.Where(entry => entry.User.Id == currentUser.Id).ToList();
+			ViewBag.FlavorId = new SelectList(userFlavors, "FlavorId", "Flavor");
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> Create(Treat treat, int FlavorId)
+		{
+			var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+			var currentUser = await _userManager.FindByIdAsync(userId);
+			treat.User = currentUser;
+			_db.Treats.Add(treat);
+			_db.SaveChanges();
+      
+			if (FlavorId != 0)
+			{
+				_db.TreatFlavor.Add(new TreatFlavor() { FlavorId = FlavorId, TreatId = treat.TreatId});
+			}
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		public ActionResult Details(int id)
+		{
+			Treat thisTreat = _db.Treats
+				.Include(treat => treat.JoinEntities)
+				.ThenInclude(join => join.Recipe)
+				.FirstOrDefault(treat => treat.TreatId == id);
+			return View(thisTreat);
+		}
+
+		public async Task<ActionResult> Edit(int id)
+		{
+			Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      
+			var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+			var currentUser = await _userManager.FindByIdAsync(userId);
+
+			List<Flavor> userFlavors = _db.Flavors.Where(entry => entry.User.Id == currentUser.Id).ToList();
+			ViewBag.FlavorId = new SelectList(userFlavors, "FlavorId", "Flavor");
+
+			return View(userFlavors);
+		}
+
+		[HttpPost]
+		public ActionResult Edit(Treat treat, int FlavorId)
+		{
+			_db.Entry(treat).State = EntityState.Modified;
+			_db.SaveChanges();
+
+			foreach(TreatFlavor join in _db.TreatFlavor)
+			{
+				if(treat.TreatId == join.TreatId && FlavorId == join.FlavorId)
+				{
+					return RedirectToAction("Details", new {id = treat.TreatId});
+				}
+			}
+
+			if (FlavorId != 0)
+			{
+        _db.TreatFlavor.Add(new TreatFlavor() { FlavorId = FlavorId, TreatId = treat.TreatId});
+				_db.SaveChanges();
+			}
+			return RedirectToAction("Details", new {id = treat.TreatId});
+		}
+
+		[HttpPost]
+		public ActionResult Delete(int id)
+		{
+			Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+			_db.Treats.Remove(thisTreat);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		[HttpPost]
+		public ActionResult DeleteFlavor(int joinId)
+		{
+			TreatFlavor thisJoin = _db.TreatFlavor.FirstOrDefault(join => join.TreatFlavorId == joinId);
+			_db.TreatFlavor.Remove(thisJoin);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		public ActionResult Search(int price)
+		{
+			List<Treat> thisSearch = _db.Treats.Where(treat => treat.Price.Contains(price)).ToList();
+			ViewBag.SearchPrice = price;
+			return View(thisSearch);
+		}
+  }
+}
