0f432e6b57a10ae84c9029bfe2c82fb03dae4005
Harold_Mesa <harold.mesa@icloud.com>
2022-10-22 11:54:19 -0500

    Add final views and refactor HomeController.cs>

--- Pierre.Solution/Controllers/HomeController.cs
+++ Pierre.Solution/Controllers/HomeController.cs
@@ -1,13 +1,26 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using PierresBakery.Models;
 
 namespace PierresBakery.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly PierresBakeryContext _db;
+
+    public HomeController(PierresBakeryContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index()
     {
-      return View();
+      ViewBag.Treat = new List<Treat>(_db.Treats);
+      return View(_db.Flavors.ToList());
     }
   }
   
